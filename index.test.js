48668a26b8c301f1165dc40e5b070fa175aa0464
Kaique da Silva <kaeyosthaeron@gmail.com>
2021-02-24 11:31:49 -0300

    Tested the sliced logic

--- index.test.js
+++ index.test.js
@@ -1,11 +1,24 @@
 'use strict';
+const cronos = require('./');
 
-jest.useFakeTimers();
+// jest.useFakeTimers();
 
-test('waits interval finish', () => {
-  const cronos = require('./');
+// test('waits interval finish', () => {
+//   const cronos = require('./');
+//
+//   cronos.start('0:00:5');
+//
+//   expect(setInterval).toHaveBeenCalledTimes(5);
+// });
 
-  cronos.start('0:00:5');
+test('give a specific time for second', () => {
+  expect(cronos.give('second')).toBe(1000);
+});
 
-  expect(setInterval).toHaveBeenCalledTimes(5);
+test('give a specific time for minute', () => {
+  expect(cronos.give('minute')).toBe(1000 * 60);
+});
+
+test('give a specific time for hour', () => {
+  expect(cronos.give('hour')).toBe((1000 * 60) * 60);
 });
